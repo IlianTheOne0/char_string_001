6887c08f26fb7a246482ff7e959afb79e29b0d31
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-27 20:15:00 +0200

    Fixed

--- main.cpp
+++ main.cpp
@@ -7,7 +7,54 @@ using std::cin;
 
 int main()
 {
+    char value[] = "LOREM123465798ipsum!@#dolor$%^sit&*(AMET)";
 
-	char ch = _getch();
-	return 0;
+    cout << "Input: " << endl << value << endl << endl;
+
+    unsigned long long i = 0;
+    while (true)
+    {
+        if ((value[i] < 'a' || value[i] > 'z') && (value[i] < 'A' || value[i] > 'Z'))
+        {
+            value[i] = ' ';
+        }
+
+        i++;
+
+        if (value[i] == '\0')
+        {
+            break;
+        }
+    }
+
+    i = 0;
+
+    unsigned long long words = 0;
+    bool inWord = false;
+    while (true)
+    {
+        if (value[i] != ' ' && !inWord)
+        {
+            words++;
+            inWord = true;
+        }
+        else if (value[i] == ' ')
+        {
+            inWord = false;
+        }
+
+        i++;
+
+        if (value[i] == '\0')
+        {
+            break;
+        }
+    }
+
+    cout << "Output: " << endl
+        << "Words: " << words << endl
+        << endl;
+
+    char ch = _getch();
+    return 0;
 }
