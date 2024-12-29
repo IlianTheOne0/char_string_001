3187652516e8f7ec04ae262ff52116b4c82f484c
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-29 22:05:38 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -15,6 +15,13 @@ void incorrect()
     SetConsoleTextAttribute(hConsole, 7);
 }
 
+template<typename TValue>
+void delete_value(TValue*& value)
+{
+    delete[] value;
+    value = nullptr;
+}
+
 template<typename TValueL, typename TValueR>
 void print(TValueL value, TValueR message)
 {
@@ -35,25 +42,36 @@ TReturn get_size(TValue value)
 }
 
 template<typename TReturn, typename TValue>
-TReturn get_value(TValue value)
+TReturn get_value(TValue value, bool indicator)
 {
     TReturn size = get_size<TReturn, const TValue>(value);
     TReturn input;
 
-    cout << "Enter the character you want to delete: ";
+    cout << "Enter the character you want to insert: ";
     cin >> input;
     cout << endl;
 
     return input;
 }
 template<typename TReturn, typename TValue>
-TReturn get_value()
+TReturn get_value(TValue value)
 {
+    TReturn size = get_size<TReturn, const TValue>(value);
     TReturn input;
 
-    cout << "Enter the character you want to delete: ";
-    cin >> input;
-    cout << endl;
+    do
+    {
+        cout << "Enter the index of the character you want to insert: ";
+        cin >> input;
+
+        if (input < 0 || input >= size + 1)
+        {
+            incorrect();
+            continue;
+        }
+        cout << endl;
+        break;
+    } while (true);
 
     return input;
 }
@@ -61,34 +79,38 @@ TReturn get_value()
 template<typename TValue>
 void inserter(TValue& value)
 {
-    char symbol = get_value<char, TValue>(value);
+    char symbol = get_value<char, TValue>(value, true);
+    unsigned long long symbol_index = get_value<unsigned long long, TValue>(value);
     unsigned long long size = get_size<unsigned long long, const TValue>(value);
 
-    unsigned long long index = 0;
-    for (unsigned long long i = 0; i < size; i++)
-    {
-        if (value[i] == symbol)
-        {
-            index++;
-        }
-        else
-        {
-            value[i - index] = value[i];
-        }
-    }
+    char* new_value = new char[size + 2];
 
-    value[size - index] = '\0';
+    for (unsigned long long i = 0; i < symbol_index; i++)
+    {
+        new_value[i] = value[i];
+    }
+    new_value[symbol_index] = symbol;
+    for (unsigned long long i = symbol_index; i < size; i++)
+    {
+        new_value[i + 1] = value[i];
+    }
+    new_value[size + 1] = '\0';
+
+    delete_value(value);
+    value = new_value;
 }
 
 int main()
 {
-    char value[] = "Hello world";
+    char* value = new char[100] {"Hello world!"};
 
     print<const char*, const char*>(value, "Input:\nSentence: ");
     print<unsigned long long, const char*>(get_size<unsigned long long, const char*>(value), "Range: from 0 to ");
     cout << endl;
     inserter(value);
     print<const char*, const char*>(value, "Output: ");
+
+    delete[] value;
 
     char ch = _getch();
     return 0;
