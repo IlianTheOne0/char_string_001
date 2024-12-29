743946adc96fe114531336f9c78522ce33e67ef0
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-29 19:32:09 +0200

    add

--- main.cpp
+++ main.cpp
@@ -7,7 +7,28 @@ using std::cin;
 
 int main()
 {
+    char value[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
-	char ch = _getch();
-	return 0;
+    cout << "Input: " << endl << value << endl << endl;
+
+    unsigned long long i = 0;
+    while (true)
+    {
+        if (value[i] == ' ')
+        {
+            value[i] = '\t';
+        }
+
+        i++;
+
+        if (value[i] == '\0')
+        {
+            break;
+        }
+    }
+
+    cout << "Output: " << endl << value << endl << endl;
+
+    char ch = _getch();
+    return 0;
 }
