3d770fa3b244c884bec358aa711caf77b30209c3
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-29 22:33:27 +0200

    Completed Task 6

--- main.cpp
+++ main.cpp
@@ -5,10 +5,24 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+template<typename TValue>
+void delete_value(TValue*& value)
+{
+    delete[] value;
+    value = nullptr;
+}
+
 template<typename TValueL, typename TValueR>
 void print(TValueL value, TValueR message)
 {
     cout << message << value << endl;
+}
+
+template<typename TValue>
+void get_value(TValue* value)
+{
+    cout << "Enter a sentence (max 100 characters): ";
+    cin.getline(value, 100);
 }
 
 template<typename TReturn, typename TValue>
@@ -42,18 +56,20 @@ void counter(TValue value, TReturn& letters, TReturn& digits, TReturn& symbols)
 
 int main()
 {
-    char value[] = "abcdefghij ABCDEFGHIJ 1234567890 !@#$%^&*()";
+    char* value = new char[100];
+    get_value<>(value);
 
     unsigned long long letters = 0;
     unsigned long long digits = 0;
     unsigned long long symbols = 0;
 
-    print<const char*, const char*>(value, "Input: ");
     counter<unsigned long long, char*>(value, letters, digits, symbols);
     cout << endl << "Output: " << endl;
     print<unsigned long long, const char*>(letters, "\tLetters: ");
     print<unsigned long long, const char*>(digits, "\tDigits: ");
     print<unsigned long long, const char*>(symbols, "\tSymbols: ");
+
+    delete_value(value);
 
     char ch = _getch();
     return 0;
